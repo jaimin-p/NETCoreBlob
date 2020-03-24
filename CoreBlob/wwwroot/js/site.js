4319b53f9e61f74e09a8410ce866a7c3341b1c6d
Jaimin <jaiminece@gmail.com>
2020-03-23 22:48:16 -0400

    Ajax Overlay Added

--- CoreBlob/wwwroot/js/site.js
+++ CoreBlob/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $.LoadingOverlaySetup({
+        background: "rgba(0, 0, 0, 0.5)",
+        imageAnimation: "2s rotate_right",
+        imageColor: "#169ddb"
+    });
+
+    $(document).ajaxStart(function () {
+        $.LoadingOverlay("show");
+    });
+    $(document).ajaxStop(function () {
+        $.LoadingOverlay("hide");
+    });
+
+    $("#createForm").submit(function (e) {
+        $.LoadingOverlay("show");
+    });
+
+    $("#editForm").submit(function (e) {
+        $.LoadingOverlay("show");
+    });
+});
